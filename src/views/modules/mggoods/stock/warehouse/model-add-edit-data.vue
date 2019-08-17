4987ec9c3147c1ebbdc5f4e8af18651bbf0df986
370508492@qq.com <370508492@qq.com>
2019-08-17 17:58:46 +0800

    仓库页面弹窗接口

--- src/views/modules/mggoods/stock/warehouse/model-add-edit-data.vue
+++ src/views/modules/mggoods/stock/warehouse/model-add-edit-data.vue
@@ -14,29 +14,30 @@
                 @keyup.enter.native="dataFormSubmit('addForm')"
                 label-width="120px"
         >
-            <el-form-item label="仓库名称：" prop="">
-                <el-input v-model="dataForm.name" placeholder="请输入"></el-input>
+            <el-form-item label="仓库名称：" prop="warehouseName">
+                <el-input v-model="dataForm.warehouseName" placeholder="请输入"></el-input>
             </el-form-item>
-            <el-form-item label="仓库种类：" prop="">
-                <el-select v-model="dataForm.sizeFeeling" placeholder="" style="width: 598px!important;">
-                    <el-option label="退货仓" value="">退货仓</el-option>
+            <el-form-item label="仓库种类：" prop="type">
+                <el-select v-model="dataForm.type" placeholder="请选择">
+                    <el-option label="发货仓" value="0"></el-option>
+                    <el-option label="退货仓" value="1"></el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="仓库负责人：" prop="">
+            <el-form-item label="仓库负责人：" prop="name">
                 <el-input v-model="dataForm.name" placeholder="请输入"></el-input>
             </el-form-item>
-            <el-form-item label="负责人联系方式：" prop="" :label-width="formLabelWidth">
-                <el-input v-model="dataForm.name" placeholder="请输入"></el-input>
+            <el-form-item label="负责人联系方式：" prop="phone" :label-width="formLabelWidth">
+                <el-input v-model="dataForm.phone" placeholder="请输入"></el-input>
             </el-form-item>
-            <el-form-item label="仓库所在地：" prop="desc" :label-width="formLabelWidth">
+            <el-form-item label="仓库所在地：" prop="addressInfo" :label-width="formLabelWidth">
                 <el-select
                         disabled
-                        v-model="dataForm.memberAreaid"
+                        v-model="dataForm.provinceId"
                         placeholder="省"
                         loading-text="加载中···"
                         @visible-change="getGoodsClass(0)"
                 >
-                    <!-- 市级 -->
+                    <!-- 省级 -->
                     <el-option
                             v-for="item in goodscalssOption0"
                             :key="item.id"
@@ -46,7 +47,7 @@
                 </el-select>
                 <el-select
                         disabled
-                        v-model="dataForm.memberCityid"
+                        v-model="dataForm.cityId"
                         placeholder="市"
                         @visible-change="getGoodsClass(1)"
                 >
@@ -59,7 +60,7 @@
                 </el-select>
                 <el-select
                         disabled
-                        v-model="dataForm.memberProvinceid"
+                        v-model="dataForm.areaId"
                         placeholder="县"
                         @visible-change="getGoodsClass(2)"
                 >
@@ -94,6 +95,7 @@
 </template>
 
 <script>
+    import { backScanWare,addWare, updataWare } from '@/api/api'
     export default {
         name: "model-add-edit-data",
         data () {
@@ -101,10 +103,10 @@
                 visible : false,
                 loading : false,
                 dataForm: {
-                    memberAreaid: "",
-                    memberCityid: "",
-                    memberProvinceid: "",
-                    stressId: ""
+                    provinceId: "",
+                    cityId: "",
+                    areaId: "",
+                    streetId: ""
                 },
                 goodscalssOption0: [],
                 goodscalssOption1: [],
@@ -144,6 +146,66 @@
             },
             closeDialog() {
                 this.$parent.addEditDataVisible = false;
+            },
+            // 编辑回显
+            backScan(){
+                var obj  = {
+                    warehouseName:this.row.warehouseName,
+                    type:this.row.type,
+                    name:this.row.name,
+                    phone:this.row.phone
+                }
+                backScanWare(obj).then((res)=>{
+                    if(res.code == 200){
+                        Object.assign(this.dataForm,res.data);
+
+                    }else{
+
+                    }
+                })
+            },
+            // 提交
+            dataFormSubmit(formName){
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.loading = true;
+                        var obj={
+                            "warehouseName": this.dataForm.warehouseName,
+                            "type": this.dataForm.type,
+                            "name": this.dataForm.name,
+                            "phone":this.dataForm.phone,
+                            // "brandValueDTOList": this.dataForm.brandValueDTOList,
+                            // "brandApply": 0,
+                            // "brandSort": 0,
+                            // "storeId": 0
+                        }
+
+                        if(this.row) obj.id = this.row.id
+                        var fn = this.row?updataWare:addWare;
+                        fn(obj).then((res) => {
+                            this.loading = false;
+                            // alert(JSON.stringify(res));
+                            let status = null;
+                            if(res.code == "200"){
+                                status = "success";
+                                this.visible = false;
+                                this.$emit('searchDataList');
+                                this.closeDialog();
+                            }else{
+                                status = "error";
+                            }
+
+                            this.$message({
+                                message: res.msg,
+                                type: status,
+                                duration: 1500
+                            })
+                        })
+                    } else {
+                        //console.log('error 添加失败!!');
+                        return false;
+                    }
+                })
             },
         }
     }
