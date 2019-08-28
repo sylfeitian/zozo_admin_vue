ec92bfe5d5b0a8bf7b1acc77ef347797fc9f0d0e
Your_username <18332116379@163.com>
2019-08-28 18:15:38 +0800

    联调中国分类接口

--- src/components/model-photo-cropper.vue
+++ src/components/model-photo-cropper.vue
@@ -41,7 +41,6 @@
 		      class="upload-demo"
 		      drag
 		      :action="action"
-		      :auto-upload="false"
 			   v-if="!cropper.imgShow"
 		    >
 			      <i class="el-icon-upload" :style="{fontSize:fontSize,lineHeight:lineHeight}"></i>
