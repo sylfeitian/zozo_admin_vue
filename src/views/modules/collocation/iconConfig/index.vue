6d4b46e13aaa4248afac99f2cc3c8a70517cde86
cxj-9280 <cxj-9280@qq.com>
2019-09-02 14:16:11 +0800

    完成广告位配置页面

---
+++ src/views/modules/collocation/iconConfig/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <Bread :breaddata="breaddata"></Bread>
+    <el-table
+	  :data="dataList"
+      v-loading="dataListLoading"
+      border
+	  style="width: 100%">
+	    <el-table-column
+	    	type="index"
+		    prop="$index"
+				align="center"
+		    label="序号"
+		    width="70">
+		    <template slot-scope="scope">
+            {{scope.$index+1+(parseInt(page)-1)* parseInt(limit) }}
+            </template>
+		</el-table-column>
+		<el-table-column
+		    prop="id"
+		    label="模块名称">
+		</el-table-column>
+        <el-table-column
+		    prop="account"
+		    label="选中图标"
+		    width="120">
+		</el-table-column>
+		<el-table-column
+		    prop="account"
+		    label="未选中图标"
+		    width="120">
+		</el-table-column>		
+	    <el-table-column
+	   		prop="address"
+	    	label="操作">
+		    <template slot-scope="scope">
+		    	<el-button type="text" size="small" @click="addActivity(scope.row.id)">编辑</el-button>
+		    </template>
+	  	</el-table-column>
+	</el-table>
+	<!-- 分页 -->
+    <el-pagination
+	    @size-change="pageSizeChangeHandle"
+	    @current-change="pageCurrentChangeHandle"
+	    :current-page="page"
+	    :page-sizes="[10, 20, 50, 100]"
+	    :page-size="limit"
+	    :total="total"
+	    layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+    import mixinViewModule from '@/mixins/view-module'
+    import { storeGrade } from '@/api/api'
+    import Bread from "@/components/bread";
+    
+    export default {
+    mixins: [mixinViewModule],
+    components:{Bread},
+        data () {
+            return {
+                mixinViewModuleOptions: {
+                    getDataListURL: businessPageUrl,
+                    getDataListIsPage: true,
+                    exportURL: '/admin-api/store/export',
+                    deleteURL: '/admin-api/store',
+                    deleteIsBatch: true,
+                    // deleteIsBatchKey: 'id'
+                },
+                buttonStatus:false,
+                activiVisible:false,
+                breaddata: ["配置管理", "底部icon配置"],
+            }
+        },
+        created(){
+            this.demo();
+        },
+        methods: {
+
+
+
+
+
+            demo(){
+                function placeholderPic(){
+                    var w = document.documentElement.offsetWidth;
+                    document.documentElement.style.fontSize=w/20+'px';
+                }
+                    placeholderPic();
+                window.onresize=function(){
+                    placeholderPic();
+                }
+            },
+        }
+    };
+</script>
+<style lang="scss" scoped>
+.el-input {
+  width: 170px;
+  height: 40px;
+}
+.activiDialog{
+    .el-input {
+        width: 300px;
+    }
+}
+
+</style>
