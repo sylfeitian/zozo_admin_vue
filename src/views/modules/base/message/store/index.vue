9667915bcac903eeef7e185a009ada283f048aff
unknown <505309708@qq.com>
2019-09-11 12:56:05 +0800

    店铺管理营业状态加个全部

--- src/views/modules/base/message/store/index.vue
+++ src/views/modules/base/message/store/index.vue
@@ -164,6 +164,7 @@ export default {
       multipleSelection: [],
       row: "",
       operateShopStore: [
+         { id: "", name: "全部" },
         { id: "1", name: "营业中" },
         { id: "2", name: "已停业" },
         { id: "0", name: "待营业" }
