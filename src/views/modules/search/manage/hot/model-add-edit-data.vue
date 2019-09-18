7c62b11bec9f7b1b8a10e991a599f1d8873c00a0
370508492@qq.com <370508492@qq.com>
2019-09-18 09:55:26 +0800

    修改搜索模块弹窗按钮布局

--- src/views/modules/search/manage/hot/model-add-edit-data.vue
+++ src/views/modules/search/manage/hot/model-add-edit-data.vue
@@ -21,12 +21,17 @@
                 <el-input v-model="dataForm.sort" placeholder="0"></el-input>
                 <span style="color: #999999;">数字越大越靠前</span>
             </el-form-item>
-            <el-form-item style="text-align: center;margin-left: -120px!important;">
-                <el-button type="primary" @click="dataFormSubmit('addForm')"
-                           :loading="loading">{{loading ? "提交中···" : "确定"}}</el-button>
-                <el-button  @click="dataFormCancel()">返回</el-button>
-            </el-form-item>
+<!--            <el-form-item style="text-align: center;margin-left: -120px!important;">-->
+<!--                <el-button type="primary" @click="dataFormSubmit('addForm')"-->
+<!--                           :loading="loading">{{loading ? "提交中···" : "确定"}}</el-button>-->
+<!--                <el-button  @click="dataFormCancel()">返回</el-button>-->
+<!--            </el-form-item>-->
         </el-form>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dataFormCancel()">取消</el-button>
+            <el-button type="primary" @click="dataFormSubmit('addForm')"
+                       :loading="loading">{{loading ? "提交中···" : "确定"}}</el-button>
+        </span>
     </el-dialog>
 </template>
 
