4aa459ba824d3b661323a1b09ad61b98ee986f28
mengjiaqiong <1322761926@qq.com>
2019-09-24 16:57:04 +0800

    1.库存底下的所有新增编辑规则校验 2.表格列数据超出部分。。。显示

--- src/views/modules/stockkc/stock/warehouse/model-add-edit-data.vue
+++ src/views/modules/stockkc/stock/warehouse/model-add-edit-data.vue
@@ -27,7 +27,7 @@
                 <el-input v-model="dataForm.name" placeholder="请输入"></el-input>
             </el-form-item>
             <el-form-item label="负责人联系方式：" prop="phone" :label-width="formLabelWidth">
-                <el-input v-model="dataForm.phone" placeholder="请输入"></el-input>
+                <el-input v-model="dataForm.phone" placeholder="请输入" maxlength="20"></el-input>
             </el-form-item>
             <el-form-item label="仓库所在地：" prop="addressInfo" :label-width="formLabelWidth">
                 <el-select v-model="dataForm.provinceId" placeholder="省"
@@ -69,13 +69,14 @@
                             :value="item.id">
                     </el-option>
                 </el-select>
-                <el-input type="textarea" v-model="dataForm.addressInfo" placeholder="请输入详细地址" :rows="4" style="margin-top: 20px;"></el-input>
+                <el-input type="textarea" v-model="dataForm.addressInfo" placeholder="请输入详细地址" :rows="4"
+                          style="margin-top: 20px;"></el-input>
             </el-form-item>
-<!--            <el-form-item style="text-align: center;margin-left: -120px!important;">-->
-<!--                <el-button  @click="dataFormCancel()">取消</el-button>-->
-<!--                <el-button type="primary" @click="dataFormSubmit('addForm')"-->
-<!--                           :loading="loading">{{loading ? "提交中···" : "确定"}}</el-button>-->
-<!--            </el-form-item>-->
+            <!--            <el-form-item style="text-align: center;margin-left: -120px!important;">-->
+            <!--                <el-button  @click="dataFormCancel()">取消</el-button>-->
+            <!--                <el-button type="primary" @click="dataFormSubmit('addForm')"-->
+            <!--                           :loading="loading">{{loading ? "提交中···" : "确定"}}</el-button>-->
+            <!--            </el-form-item>-->
         </el-form>
         <span slot="footer" class="dialog-footer">
             <el-button @click="dataFormCancel()">取消</el-button>
@@ -86,23 +87,24 @@
 </template>
 
 <script>
-    import { backScanWare,addWare, updataWare} from '@/api/api'
-    import { areaFirst, areaByParentId, verifyWare } from "@/api/api"
+    import {backScanWare, addWare, updataWare} from '@/api/api'
+    import {areaFirst, areaByParentId, verifyWare} from "@/api/api"
+
     export default {
         name: "model-add-edit-data",
-        data () {
+        data() {
             var validatorWarehouseName = (rule, value, callback) => {
                 if (value != '') {
-                     var obj  ={
-                        params:{
-                            id:this.row?this.row.id:"",
-                            name:value
+                    var obj = {
+                        params: {
+                            id: this.row ? this.row.id : "",
+                            name: value
                         }
-                     }
-                    verifyWare(obj).then((res)=>{
-                        if(res){
+                    }
+                    verifyWare(obj).then((res) => {
+                        if (res) {
                             callback()
-                        }else{
+                        } else {
                             callback(new Error('仓库名称已经存在'))
                         }
                     })
@@ -112,68 +114,105 @@
                 }
             }
             return {
-                visible : false,
-                loading : false,
+                visible: false,
+                loading: false,
                 dataForm: {
                     provinceId: "",
-                    provinceName:"",
+                    provinceName: "",
                     cityId: "",
-                    cityName:"",
+                    cityName: "",
                     areaId: "",
-                    areaName:"",
+                    areaName: "",
                     streetId: "",
-                    streetName:"",
-                    warehouseName:"",
-                    name:"",
-                    phone:"",
-                    type:"",
-                    addressInfo:""
+                    streetName: "",
+                    warehouseName: "",
+                    name: "",
+                    phone: "",
+                    type: "",
+                    addressInfo: ""
 
                 },
-                optionsArea1:[],
-                optionsArea2:[],
-                optionsArea3:[],
-                optionsArea4:[],
-                dataRule : {
-                    warehouseName : [
-                        { required: true, message: '必填项不能为空', trigger: 'blur' },
-                        { validator: validatorWarehouseName, trigger: 'blur'}
+                optionsArea1: [],
+                optionsArea2: [],
+                optionsArea3: [],
+                optionsArea4: [],
+                dataRule: {
+                    warehouseName: [
+                        {required: true, message: '必填项不能为空', trigger: 'blur'},
+                        {validator: validatorWarehouseName, trigger: 'blur'}
                     ],
-                    type : [
-                        { required: true, message: '必填项不能为空', trigger: 'blur' },
+                    type: [
+                        {required: true, message: '必填项不能为空', trigger: 'blur'},
                     ],
-                    name : [
-                        { required: true, message: '必填项不能为空', trigger: 'blur' },
+                    name: [
+                        {required: true, message: '必填项不能为空', trigger: 'blur'},
                     ],
-                    phone : [
-                        { required: true, message: '必填项不能为空', trigger: 'blur' },
+                    phone: [
+                        {required: true, message: '必填项不能为空', trigger: 'blur'},
                     ],
-                    addressInfo : [
-                        { required: true, message: '必填项不能为空', trigger: 'blur' },
+                    addressInfo: [
+                        {required: true, message: '必填项不能为空', trigger: 'blur'},
                     ]
                 },
                 optionsApplication: [],
                 optionsRight: [],
-                title:'',
-                orderBase:{},
-                row:"",
-                receiverInfo:{},
+                title: '',
+                orderBase: {},
+                row: "",
+                receiverInfo: {},
                 formLabelWidth: '120px'
             }
         },
+        watch: {
+            'dataForm.warehouseName': function (newV, oldV) {
+                var chineseCount = 0, characterCount = 0;
+                for (let i = 0; i < newV.length; i++) {
+                    if (/^[\u4e00-\u9fa5]*$/.test(newV[i])) { //汉字
+                        chineseCount = chineseCount + 2;
+                    } else { //字符
+                        characterCount = characterCount + 1;
+                    }
+                    var count = chineseCount + characterCount;
+                    if (count > 100) { //输入字符大于100的时候过滤
+                        this.dataForm.warehouseName = newV.substr(0, (chineseCount / 2 + characterCount) - 1)
+                    }
+                }
+            },
+            'dataForm.name': function (newV, oldV) {
+                var chineseCount = 0, characterCount = 0;
+                for (let i = 0; i < newV.length; i++) {
+                    if (/^[\u4e00-\u9fa5]*$/.test(newV[i])) { //汉字
+                        chineseCount = chineseCount + 2;
+                    } else { //字符
+                        characterCount = characterCount + 1;
+                    }
+                    var count = chineseCount + characterCount;
+                    if (count > 20) { //输入字符大于20的时候过滤
+                        this.dataForm.name = newV.substr(0, (chineseCount / 2 + characterCount) - 1)
+                    }
+                }
+            },
+            'dataForm.phone': function (newV, oldV) {
+                for (let i = 0; i < newV.length; i++) {
+                    if (!/[0-9|\-]/g.test(newV[i])) { //数字、字母
+                        this.dataForm.phone = newV.replace(newV[i], "")
+                    }
+                }
+            }
+        },
         methods: {
-            init (row) {
+            init(row) {
                 var orderBase = row.orderBase
                 var receiverInfo = row.receiverInfo
                 console.log(row)
                 this.visible = true;
                 this.row = row;
                 console.log(row)
-                if(row){
-                    this.title="编辑仓库";
+                if (row) {
+                    this.title = "编辑仓库";
                     this.backScan();
-                }else{
-                    this.title="添加仓库"
+                } else {
+                    this.title = "添加仓库"
 
                 }
                 //获取地址一级下拉
@@ -184,56 +223,56 @@
                     // this.getApplyPullList();
                 })
             },
-            getFirstData(){
-                areaFirst().then((res)=>{
+            getFirstData() {
+                areaFirst().then((res) => {
                     console.log(res);
-                    if(res.code==200){
+                    if (res.code == 200) {
                         this.optionsArea1 = res.data;
                     }
                 })
             },
-            changeArea(id,argu2,reset=true){
-                if(!id){
+            changeArea(id, argu2, reset = true) {
+                if (!id) {
                     return
                 }
-                var obj ={
-                    id:id
+                var obj = {
+                    id: id
                 }
-                if(argu2==1 && reset){
+                if (argu2 == 1 && reset) {
                     this.dataForm.cityId = "";
                     this.dataForm.areaId = "";
                     this.dataForm.streetId = "";
                     this.optionsArea2 = []
                     this.optionsArea3 = []
                     this.optionsArea4 = []
-                }else if(argu2==2 && reset){
+                } else if (argu2 == 2 && reset) {
                     this.dataForm.areaId = "";
                     this.dataForm.streetId = "";
                     this.optionsArea3 = []
                     this.optionsArea4 = []
-                }else if(argu2==3 && reset){
+                } else if (argu2 == 3 && reset) {
                     this.dataForm.streetId = "";
                     this.optionsArea4 = []
-                }else if(argu2==4 && reset){
+                } else if (argu2 == 4 && reset) {
                     // 选到最后一级了
                 }
-                areaByParentId(obj).then((res)=>{
+                areaByParentId(obj).then((res) => {
                     console.log(res);
-                    if(res.code==200){
-                        if(argu2==1){
+                    if (res.code == 200) {
+                        if (argu2 == 1) {
                             this.optionsArea2 = res.data;
-                        }else if(argu2==2){
+                        } else if (argu2 == 2) {
                             this.optionsArea3 = res.data;
-                        }else if(argu2==3){
+                        } else if (argu2 == 3) {
                             this.optionsArea4 = res.data;
-                        }else if(argu2==4){
+                        } else if (argu2 == 4) {
                             // 选到最后一级了
 
                         }
                     }
                 })
             },
-            dataFormCancel(){
+            dataFormCancel() {
                 this.visible = false;
                 this.closeDialog();
             },
@@ -241,60 +280,60 @@
                 this.$parent.addEditDataVisible = false;
             },
             // 编辑回显
-            backScan(){
-                var obj  = {
-                    id:this.row.id,
+            backScan() {
+                var obj = {
+                    id: this.row.id,
                 }
-                backScanWare(obj).then((res)=>{
-                    if(res.code == 200){
-                        Object.assign(this.dataForm,res.data);
+                backScanWare(obj).then((res) => {
+                    if (res.code == 200) {
+                        Object.assign(this.dataForm, res.data);
                         this.preOptionsArea1 = [];
                         // this.orderBase = orderBase;
                         // Object.assign(this.dataForm,receiverInfo);
-                        var receiverInfo  = res.data
+                        var receiverInfo = res.data
                         // this.preOptionsArea1 = [];
-                        this.optionsArea2[0] = {id:receiverInfo.cityId,name:receiverInfo.cityName};
-                        this.optionsArea3[0] = {id:receiverInfo.areaId,name:receiverInfo.areaId};
-                        this.optionsArea4[0] =  {id:receiverInfo.streetId,name:receiverInfo.streetName};
+                        this.optionsArea2[0] = {id: receiverInfo.cityId, name: receiverInfo.cityName};
+                        this.optionsArea3[0] = {id: receiverInfo.areaId, name: receiverInfo.areaId};
+                        this.optionsArea4[0] = {id: receiverInfo.streetId, name: receiverInfo.streetName};
                         // receiverInfo.provinceId && this.changeArea(receiverInfo.provinceId,1,false);
                         // receiverInfo.cityId && this.changeArea(receiverInfo.cityId,2,false);
                         // receiverInfo.areaId && this.changeArea(receiverInfo.areaId,3,false);
 
-                    }else{
+                    } else {
 
                     }
                 })
             },
             // 提交
-            dataFormSubmit(formName){
+            dataFormSubmit(formName) {
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
                         this.loading = true;
-                        var obj={
+                        var obj = {
                             warehouseName: this.dataForm.warehouseName,
                             type: this.dataForm.type,
                             name: this.dataForm.name,
-                            phone:this.dataForm.phone,
+                            phone: this.dataForm.phone,
                             areaId: this.dataForm.areaId,
                             cityId: this.dataForm.cityId,
                             provinceId: this.dataForm.provinceId,
                             streetId: this.dataForm.streetId,
-                            addressInfo: this.dataForm.addressInfo ,
+                            addressInfo: this.dataForm.addressInfo,
                             // isEnable: 1
                         }
                         console.log(obj)
-                        if(this.row) obj.id = this.row.id;
-                        var fn = this.row?updataWare:addWare;
+                        if (this.row) obj.id = this.row.id;
+                        var fn = this.row ? updataWare : addWare;
                         fn(obj).then((res) => {
                             this.loading = false;
                             // alert(JSON.stringify(res));
                             let status = null;
-                            if(res.code == "200"){
+                            if (res.code == "200") {
                                 status = "success";
                                 this.visible = false;
                                 this.$emit('searchDataList');
                                 this.closeDialog();
-                            }else{
+                            } else {
                                 status = "error";
                             }
 
@@ -316,12 +355,15 @@
 
 <style lang="scss" scoped>
     /deep/ .el-form-item__label {
-        width: 130px!important;
+        width: 130px !important;
     }
-    /deep/ .el-form-item__content{
-        margin-left: 130px!important;
+
+    /deep/ .el-form-item__content {
+        margin-left: 130px !important;
     }
-    /deep/ .el-select--default{
-        width: 146px!important;
+
+    /deep/ .el-select--default {
+        width: 146px !important;
     }
+
 </style>
