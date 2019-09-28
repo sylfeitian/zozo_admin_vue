7095da42755e0773e217afa6f3ec60f5b06797c8
mengjiaqiong <1322761926@qq.com>
2019-09-28 11:19:34 +0800

    店铺主风格标签 超出。。。提示

--- src/views/modules/base/message/store/index.vue
+++ src/views/modules/base/message/store/index.vue
@@ -64,7 +64,13 @@
         </template>
       </el-table-column>
       <el-table-column prop="updateDate" label="更新时间" align="center"></el-table-column>
-      <el-table-column prop="mainTag" label="店铺主风格标签" align="center"></el-table-column>
+      <el-table-column prop="mainTag" label="店铺主风格标签" align="center">
+        <template slot-scope="scope">
+          <div :title="scope.row.mainTag">
+            {{scope.row.mainTag}}
+          </div>
+        </template>
+      </el-table-column>
       <el-table-column prop="subTag" label="店铺副风格标签" align="center">
         <template slot-scope="scope">
           <div :title="scope.row.subTag">
