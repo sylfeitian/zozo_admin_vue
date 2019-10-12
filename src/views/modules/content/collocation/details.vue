95c2914c7910bb128f37c18df9abdb52c8b5450b
guoxuekun123 <7984979484@qq.com>
2019-10-12 17:45:52 +0800

    禅道内容模块bug

--- src/views/modules/content/collocation/details.vue
+++ src/views/modules/content/collocation/details.vue
@@ -108,7 +108,7 @@
                     <span>{{dataForm.totalFavNum}}</span>
                 </el-form-item>
                 <el-form-item label="发布状态：">
-                    <span>{{dataForm.state == 2?"取消发布":dataForm.state == 1?"已发布":""}}</span>
+                    <span>{{dataForm.state == 2?"取消发布":dataForm.state == 1?"已发布":dataForm.state == 0?"未发布":""}}</span>
                 </el-form-item>
                 <el-form-item label="主图：" style="height: 100%!important;">
                     <template slot-scope="scope">
