8debe2cd9037ec64fea06ac67ce586c509cfa222
fengxuewei <fengxuewei@leimingtech.com>
2019-10-18 09:31:07 +0800

    商品详情

--- src/views/modules/mggoods/good/goodslist/detail.vue
+++ src/views/modules/mggoods/good/goodslist/detail.vue
@@ -252,7 +252,6 @@
                 // })
             },
             init(row){
-                text
                 this.row = row;
                 this.$nextTick(()=>{
                     if(row){
