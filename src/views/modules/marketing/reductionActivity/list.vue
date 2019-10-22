67bdf29c9e6d1a5cdcce217ee7e76cf36ee8b61b
mengjiaqiong <1322761926@qq.com>
2019-10-22 16:44:37 +0800

    bug编号：21606 描述：活动状态和审核状态输入框默认显示全部 解决方案：修改重置方法，挨个置空

--- src/views/modules/marketing/reductionActivity/list.vue
+++ src/views/modules/marketing/reductionActivity/list.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Bread :breaddata="breaddata"></Bread>
-    <el-form :inline="true" class="grayLine topGapPadding" :model="dataForm" @keyup.enter.native="getDataList()" >
+    <el-form :inline="true" class="grayLine topGapPadding" ref="dataForm" :model="dataForm" @keyup.enter.native="getDataList()" >
         <el-form-item label="活动标题：">
             <el-input v-model.trim="dataForm.title" placeholder="请输入活动标题" clearable></el-input>
         </el-form-item>
@@ -41,7 +41,7 @@
         
         <el-form-item>
             <el-button  class="btn" type="primary" @click="getData()">搜索</el-button>
-            <el-button class="btn"  type="primary" plain @click="reset()" plain>重置</el-button>
+            <el-button class="btn"  type="primary" plain @click="reset('dataForm')" plain>重置</el-button>
         </el-form-item>
         <br />
         <el-form-item>
@@ -242,8 +242,14 @@
                 this.deleteHandle(row.id);
             },
             //重置
-            reset() {
-                this.dataForm = {};
+            reset(formName) {
+                this.dataForm.title = ""
+                this.dataForm.auditState = ''
+                this.dataForm.state = ''
+                this.valuetime = [];
+                this.dataForm.startTime = "";
+                this.dataForm.endtime = "";
+                this.$refs[formName].resetFields();
                 this.getDataList();
             },
             //打开新增编辑活动弹框
