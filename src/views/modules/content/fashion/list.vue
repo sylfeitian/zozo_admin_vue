fddfecdb4a6b02e976c49e661052f7fe68e45f0f
fengxuewei <fengxuewei@leimingtech.com>
2019-10-26 11:36:41 +0800

    bug22296

--- src/views/modules/content/fashion/list.vue
+++ src/views/modules/content/fashion/list.vue
@@ -238,6 +238,7 @@
                 this.dataForm.publishJpEndTime = "";
                 this.dataForm.publishStartTime = "";
                 this.dataForm.publishEndTime = "";
+                this.dataForm.titleOrJp = "";
                 this.page = 1;
                 this.getDataList();
             },
