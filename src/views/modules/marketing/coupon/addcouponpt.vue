7303dd098d23f8a84ab79b3f25221652e0b71b71
fengxuewei <fengxuewei@leimingtech.com>
2019-10-28 14:57:09 +0800

    bug22481:优惠券面额可以显示小数

--- src/views/modules/marketing/coupon/addcouponpt.vue
+++ src/views/modules/marketing/coupon/addcouponpt.vue
@@ -221,13 +221,41 @@ export default {
                 }
             }
         },
-        'dataForm.faceValue':function(newV,oldV) {
-            newV=~~newV;
+        // 'dataForm.faceValue':function(newV,oldV) {
+        //     newV=~~newV;
+        //     for(let i=0;i<newV.toString().length;i++){
+        //         // 只能输入数字和小数点
+        //         if(!/[0-9|\.]/g.test(newV[i])){
+        //             this.dataForm.faceValue = newV.toString().replace(newV[i],"")
+        //         }
+        //     }
+        // },
+        'dataForm.faceValue':function (newV,oldV) {
+            // newV=~~newV;
             for(let i=0;i<newV.toString().length;i++){
                 // 只能输入数字和小数点
                 if(!/[0-9|\.]/g.test(newV[i])){
                     this.dataForm.faceValue = newV.toString().replace(newV[i],"")
                 }
+            }
+            // 解决数字键盘可以输入输入多个小数点问题
+            if(newV==='' && oldV.toString().indexOf('.')>0){
+                this.dataForm.faceValue = oldV;
+                return ;
+            }
+            // 保留两位小数
+            if(newV){
+                newV = newV.toString();
+                var pointIndex =  newV.indexOf('.');
+                if(pointIndex>0 && (newV.length - pointIndex)>3){
+                    this.dataForm.faceValue = oldV;
+                    return ;
+                }
+            }
+            // 最大值
+            if(newV>1000000){
+                this.dataForm.faceValue = oldV;
+                return ;
             }
         },
         'dataForm.threshold':function(newV,oldV) {
@@ -261,7 +289,8 @@ export default {
                     this.dataForm.bei = newV.substr(0, (chineseCount / 2 + characterCount) - 1)
                 }
             }
-        }
+        },
+        
     },
     created(){
         console.log('999999',this.type,this.editSatusId)
