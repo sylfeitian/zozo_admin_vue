a45daffbee1ef717d5002c135a6e8d2df5235feb
fengxuewei <fengxuewei@leimingtech.com>
2019-10-28 09:53:49 +0800

    bug22409:店铺名称做限制

--- src/views/modules/base/message/store/index.vue
+++ src/views/modules/base/message/store/index.vue
@@ -195,6 +195,22 @@ export default {
       recommendFlag: ""
     };
   },
+  watch: {
+    'dataFormShow.stName':function(newV,oldV) {
+        var chineseCount = 0,characterCount = 0;
+        for (let i = 0; i < newV.length; i++) {
+            if (/^[\u4e00-\u9fa5]*$/.test(newV[i])) { //汉字
+                chineseCount = chineseCount + 2;
+            } else { //字符
+                characterCount = characterCount + 1;
+            }
+            var count = chineseCount + characterCount;
+            if (count > 300) { //输入字符大于300的时候过滤
+                this.dataFormShow.stName = newV.substr(0,(chineseCount/2+characterCount)-1)
+            }
+        }
+    },
+  },
   created() {
     // let obj = {
     //     params:{
