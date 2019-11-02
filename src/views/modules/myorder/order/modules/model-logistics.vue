3f8560ded5e5e2e845a0255bfd8717a384dd4106
fengxuewei <fengxuewei@leimingtech.com>
2019-11-02 16:07:43 +0800

    bug23224

--- src/views/modules/myorder/order/modules/model-logistics.vue
+++ src/views/modules/myorder/order/modules/model-logistics.vue
@@ -46,7 +46,10 @@ import {logisticsInfo} from "@/api/api.js"
                       this.loading = false;
                       console.log(res);
                       if(res.code==200){
-                        this.logisticsData = res.data;
+                        // this.logisticsData = res.data;
+                        res.data.forEach((item)=>{
+                            this.logisticsData.unshift(item);
+                        })
                         if(this.logisticsData.length!=0){
                             this.active = this.logisticsData.length-1
                         }
