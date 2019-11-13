ca3e85cfd206df20c7b93d7f4c2f75ad0df4873a
fengxuewei <fengxuewei@leimingtech.com>
2019-11-13 15:33:54 +0800

    bug24306

--- src/views/modules/report/afterStatistics/index.vue
+++ src/views/modules/report/afterStatistics/index.vue
@@ -95,18 +95,25 @@
             <el-table-column prop="status" label="售后状态" align="center">
                    <!-- （退货退款 10待审核、20待退货、30待入库、40待退款、50退款中、60退款完成、70退款失败、80售后取消） 仅退款（10退款中、20退款完成、30退款失败） -->
                     <template slot-scope="scope">
-                        <el-tag v-if="scope.row.auditStatus==0" type="danger">待审核</el-tag>
-                        <el-tag v-else-if="scope.row.auditStatus==2" type="danger">审核不通过</el-tag>
-                        <el-tag v-else-if="scope.row.auditStatus==3" type="danger">已取消</el-tag>
-                        <!-- 审核通过 -->
-                        <div  v-else-if="scope.row.auditStatus==1">
-                            <el-tag v-if="scope.row.status==10" type="danger">待退货</el-tag>
-                            <el-tag v-else-if="scope.row.status==20" type="danger">待入库</el-tag>
-                            <el-tag v-else-if="scope.row.status==30" type="danger">待退款</el-tag>
-                            <el-tag v-else-if="scope.row.status==40" type="danger">退款中</el-tag>
-                            <el-tag v-else-if="scope.row.status==50" type="danger">退款完成</el-tag>
-                            <el-tag v-else-if="scope.row.status==60" type="danger">退款失败</el-tag>
-                            <el-tag v-else-if="scope.row.status==70" type="danger">售后取消</el-tag>
+                        <div v-if="scope.row.aftersaleType == 0" >
+                            <el-tag v-if="scope.row.auditStatus==0" type="danger">待审核</el-tag>
+                            <el-tag v-else-if="scope.row.auditStatus==2" type="danger">审核不通过</el-tag>
+                            <el-tag v-else-if="scope.row.auditStatus==3" type="danger">已取消</el-tag>
+                            <!-- 审核通过 -->
+                            <div  v-else-if="scope.row.auditStatus==1">
+                                <el-tag v-if="scope.row.status==10" type="danger">待退货</el-tag>
+                                <el-tag v-else-if="scope.row.status==20" type="danger">待入库</el-tag>
+                                <el-tag v-else-if="scope.row.status==30" type="danger">待退款</el-tag>
+                                <el-tag v-else-if="scope.row.status==40" type="danger">退款中</el-tag>
+                                <el-tag v-else-if="scope.row.status==50" type="danger">退款完成</el-tag>
+                                <el-tag v-else-if="scope.row.status==60" type="danger">退款失败</el-tag>
+                                <el-tag v-else-if="scope.row.status==70" type="danger">售后取消</el-tag>
+                            </div>
+                        </div>
+                        <div v-if="scope.row.aftersaleType == 1" >
+                            <el-tag v-if="scope.row.status==10" type="danger">退款中</el-tag>
+                            <el-tag v-else-if="scope.row.status==20" type="danger">退款完成</el-tag>
+                            <el-tag v-else-if="scope.row.status==30" type="danger">退款失败</el-tag>
                         </div>
                     </template>
             </el-table-column>
