e7483a911f0d94b2f1239a0be754b8d8f2ca79c6
fengxuewei <fengxuewei@leimingtech.com>
2019-11-27 15:12:57 +0800

    修改商品详情列表价格展示

--- src/views/modules/mggoods/good/goodslist/detail.vue
+++ src/views/modules/mggoods/good/goodslist/detail.vue
@@ -212,8 +212,8 @@
                         </el-table-column>
                         <el-table-column prop="sellPrice" label="售价(RMB)" align="center">
                             <template slot-scope="scope">
-                                <span v-if="scope.row.specSellPrice!==''&&scope.row.specSellPrice!==null"
-                            >￥{{scope.row.sellPrice?scope.row.sellPrice:'0.00'}}</span>
+                                <span v-if="scope.row.priceType == 'proper'">￥{{scope.row.sellPrice}}</span>
+                                <span v-else-if="scope.row.priceType == 'sale'">￥{{scope.row.discountPrice}}</span>
                                 <span v-else>/</span>
                             </template>
                         </el-table-column>
