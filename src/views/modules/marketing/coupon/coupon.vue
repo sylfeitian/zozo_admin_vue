8c742f801a0b122eedbc3acd24a2ad9978fac8f5
fengxuewei <fengxuewei@leimingtech.com>
2020-01-02 18:08:00 +0800

    bug26113

--- src/views/modules/marketing/coupon/coupon.vue
+++ src/views/modules/marketing/coupon/coupon.vue
@@ -238,7 +238,10 @@ export default {
   created(){
   	this.dataForm.type = this.couponKindList1 && this.couponKindList1[0].id;
   	this.dataForm.state = this.activitesstates && this.activitesstates[0].id;
-  	this.dataForm.auditState = this.storeTypes && this.storeTypes[0].id;
+	  this.dataForm.auditState = this.storeTypes && this.storeTypes[0].id;
+  },
+  activated () {
+	  this.getData();
   },
   methods: {
 	  getData () {
